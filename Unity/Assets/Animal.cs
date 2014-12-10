b0e05081fce6aabe35f8bf62ab709c05e2630208
Jerry Fu <jerry@instituteofplay.org>
2014-12-09 18:39:13 -0800

    First idle/roam AI

---
+++ Unity/Assets/Animal.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Animal : MonoBehaviour {
+  public float Speed;
+
+  public float TurnSpeed; // Degrees per sec
+
+  public GameObject Target;
+
+  private AnimalBehaviorState m_currentState;
+
+  public Vector2 TargetPoint;
+  private float m_idleTime;
+
+  void Awake()
+  {
+    BeginIdle();
+  }
+
+  public void BeginIdle()
+  {
+    Target = null;
+    m_currentState = new Idle().Initialize(this);
+  }
+
+  void Update()
+  {
+    if (m_currentState != null)
+    {
+      m_currentState.Do();
+    }
+  }
+}
