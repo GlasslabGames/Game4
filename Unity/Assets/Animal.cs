7c674e9f92e50b1ca009db83a4a9e0fd9fa324b3
Rose Abernathy <rose@instituteofplay.org>
2014-12-15 15:19:31 -0800

    Improvements - random color, dragging creatures

--- Unity/Assets/Animal.cs
+++ Unity/Assets/Animal.cs
@@ -19,24 +19,43 @@ public class Animal : MonoBehaviour {
   private float m_idleTime;
 
   private GLDragDropContainer m_container;
-  
+
+  public UITexture BodyTexture;
+
   void Awake()
   {
     BeginIdle();
 
     m_container = GetComponent<GLDragDropContainer>();
     m_container.ItemDropped += onItemDropped;
+
+    Debug.Log (this.name+" awake!");
   }
 
   private void onItemDropped(GLDragEventArgs args)
   {
     Debug.Log(args.DragObject.name);
+    if (args.DragObject.GetComponent<FoodCrate>() != null) {
+      AnimalManager.Instance.Split(this, 3);
+      GameObject.Destroy(args.DragObject.gameObject);
+    }
   }
 
   public void BeginIdle()
   {
     Target = null;
     m_currentState = new Idle().Initialize(this);
+  }
+
+  public void SetColor(Color c, bool change = false) {
+    if (change) {
+      float d = UnityEngine.Random.Range(0.25f, 0.5f);
+      float i = UnityEngine.Random.Range(0, 3);
+      c.r = Mathf.Clamp01( c.r + ((i == 0)? d : -0.5f * d ));
+      c.g = Mathf.Clamp01( c.g + ((i == 1)? d : -0.5f * d ));
+      c.b = Mathf.Clamp01( c.b + ((i == 2)? d : -0.5f * d ));
+    }
+    BodyTexture.color = c;
   }
 
   public void HungerForMoreFood()
--- Unity/Assets/AnimalManager.cs
+++ Unity/Assets/AnimalManager.cs
@@ -17,12 +17,19 @@ public class AnimalManager : SingletonBehavior<AnimalManager>
 
     m_animals = Utility.FindInstancesInScene<Animal>();
 
+    Color[] animalColors = new Color[3]; 
+    animalColors[0] = new Color(0.8f, 0, 0);
+    animalColors[1] = new Color(0, 0.6f, 0f);
+    animalColors[2] = new Color(0, 0, 0.8f);
+
     // Add more animals to match the target number
     Animal a = m_animals[0]; // if we don't have at least one animal, we're in trouble anyway ¯\_(ツ)_/¯
+    a.SetColor(animalColors[0]);
     Animal b; Transform t;
-    while (m_animals.Count < 10) {
+    while (m_animals.Count < 3) {
       t = Utility.InstantiateAsChild(a.gameObject, a.transform.parent);
       b = t.GetComponent<Animal>();
+      b.SetColor(animalColors[m_animals.Count]);
       m_animals.Add(b);
       b.gameObject.name = "Animal"+m_animals.Count;
       t.localPosition = new Vector3( Random.Range(-500, 500), Random.Range(-400, 400));
@@ -40,6 +47,20 @@ public class AnimalManager : SingletonBehavior<AnimalManager>
 
     m_totalHappiness /= totalAnimals;
     FillBarTransform.localScale = new Vector3(m_totalHappiness, 1, 1);
+  }
+
+  // Splits a single animal a into #num animals
+  public void Split(Animal a, int num) {
+    // Later we could change this to make one big animal split into multiple small animals or something
+    for (int i = 1; i < num; i++) {
+      Transform t = Utility.InstantiateAsChild(a.gameObject, a.transform.parent);
+      Animal b = t.GetComponent<Animal>();
+      b.SetColor(a.BodyTexture.color, true);
+      m_animals.Add(b);
+      b.gameObject.name = "Animal"+m_animals.Count;
+      b.collider.enabled = false;
+      Utility.Delay(delegate{ b.collider.enabled = true; }, 1f);
+    }
   }
 
   void Update()
