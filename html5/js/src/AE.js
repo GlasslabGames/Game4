245ebab3a6eb99fc0aea601e89fb01a9e4439be6
Jerry Fu <jerry@instituteofplay.org>
2015-01-08 16:39:21 -0800

    Animation test

--- html5/js/src/AE.js
+++ html5/js/src/AE.js
@@ -8,31 +8,29 @@ AE.Animation = function(data, game)
 {
     this.data = data; // AE.Data
     this.currentFrame = 0;
+    this.elapsedTime = 0.0; // time elapsed in animation in milliseconds
     this.isPlaying = false;
     this.loop = false;
     this.reversed = false;
-    this.totalFrames = data.totalFrames;
+    this.totalFrames = parseInt(data.totalFrames);
+    this.duration = parseFloat(data.duration) * 1000.0; // duration of animation in milliseconds
 
     this.init(game);
 };
 
 AE.Animation.prototype.init = function(game)
 {
-    this.gameObject = game.add.sprite();
+    this.sprite = game.add.sprite();
+    this.renderGroup = game.make.group();
+    this.sprite.addChild(this.renderGroup);
+    this.sprite.update = this.update;
 
     var comp = this.data.composition;
-    var layers = comp.layers;
-    for (var i=0; i < layers.length; i++)
-    {
-        var layer = layers[i];
-        if (layer.type == "Footage")
-        {
-            layer.sprite = game.make.sprite(0, 0, layer.name);
-            layer.goToFrame(0);
+    comp.init(game, this.renderGroup);
+    this.sprite.addChild(comp.skeletalContainer);
+    this.renderGroup.sort('z', Phaser.Group.SORT_DESCENDING);
 
-            this.gameObject.addChild(layer.sprite);
-        }
-    }
+    this.goToFrame(0);
 };
 
 AE.Animation.prototype.play = function()
@@ -40,42 +38,46 @@ AE.Animation.prototype.play = function()
     this.isPlaying = true;
 };
 
-AE.Animation.prototype.update = function()
+AE.Animation.prototype.update = function(dt)
 {
     if (this.isPlaying)
     {
+        // Calculate current time
         if (!this.reversed)
         {
-            this.currentFrame++;
-            if (this.currentFrame > this.totalFrames)
+            this.elapsedTime += dt;
+            if (this.elapsedTime > this.duration)
             {
                 if (this.loop)
                 {
-                    this.currentFrame = 0;
+                    this.elapsedTime -= this.duration;
                 }
                 else
                 {
-                    this.currentFrame = this.totalFrames;
+                    this.elapsedTime = this.duration;
                     this.isPlaying = false;
                 }
             }
         }
         else
         {
-            this.currentFrame--;
+            this.elapsedTime -= dt;
             if (this.currentFrame < 0)
             {
                 if (this.loop)
                 {
-                    this.currentFrame = this.totalFrames;
+                    this.elapsedTime -= this.duration;
                 }
                 else
                 {
-                    this.currentFrame = 0;
+                    this.elapsedTime = 0;
                     this.isPlaying = false;
                 }
             }
         }
+
+        var normalizedTime = this.elapsedTime / this.duration;
+        this.currentFrame = parseInt(this.totalFrames * normalizedTime);
 
         this.goToFrame(this.currentFrame);
     }
@@ -83,12 +85,13 @@ AE.Animation.prototype.update = function()
 
 AE.Animation.prototype.goToFrame = function(frameNum)
 {
-
+    this.data.composition.goToFrame(frameNum);
 };
 
 AE.Data = function(xmlDom)
 {
     this.requiredAssets = {}; // Used to track what assets are required for this animation.
+    this.compByID = {}; // Used to track compositions by their ID
     if (xmlDom.children[0].nodeName == "after_affect_animation_doc") xmlDom = xmlDom.children[0];
     if (xmlDom.attributes)
     {
@@ -107,7 +110,6 @@ AE.Data = function(xmlDom)
 
         if (nodeName == "sub_items")
         {
-            this.compByID = {};
             nodeAttributes = [];
             for (var subItemIndex = 0; subItemIndex < node.children.length; subItemIndex++)
             {
@@ -171,7 +173,9 @@ AE.Composition = function(xmlDom, root)
         var child = xmlDom.children[i]; // <layer> or <meta>
         if (child.nodeName == "layer")
         {
-            this.layers.push(new AE.Layer(child, root));
+            var layer = new AE.Layer(child, root);
+            this.layers.push(layer);
+            layer.composition = this;
         }
         else if (child.nodeName == "meta")
         {
@@ -186,6 +190,71 @@ AE.Composition = function(xmlDom, root)
             console.error("Didn't know how to process node with name "+ child.nodeName);
         }
         // </layer>
+    }
+};
+
+AE.Composition.prototype.init = function(game, renderGroup)
+{
+    var layers = this.layers;
+    if (!this.skeletalContainer)
+        this.skeletalContainer = game.make.sprite();
+
+    for (var i=0; i < layers.length; i++)
+    {
+        var layer = layers[i];
+        if (!layer.sprite)
+        {
+            layer.sprite = game.make.sprite(0, 0, layer.name);
+            layer.sprite.z = i;
+            renderGroup.add(layer.sprite);
+            //layer.skeletalContainer = game.make.sprite(0,0,layer.name);
+            layer.skeletalContainer = game.make.sprite();
+        }
+
+        if (layer.type == "Footage")
+        {
+            //layer.goToFrame(0);
+
+            if (layer.parent == "none")
+            {
+                this.skeletalContainer.addChild(layer.skeletalContainer);
+            }
+            else
+            {
+                var parentLayer = layer.composition.layers[parseInt(layer.parent) - 1];
+                // TODO: Clean since this check shouldn't need to exist
+                if (!parentLayer.sprite)
+                {
+                    parentLayer.sprite = game.make.sprite(0, 0, parentLayer.name);
+                    parentLayer.sprite.z = parentLayer.id - 1;
+                    //parentLayer.skeletalContainer = game.make.sprite(0, 0, parentLayer.name);
+                    parentLayer.skeletalContainer = game.make.sprite();
+                    renderGroup.add(parentLayer.sprite);
+                }
+
+                parentLayer.skeletalContainer.addChild(layer.skeletalContainer);
+            }
+        }
+        else if (layer.type == "Composition")
+        {
+            // TODO: This doesn't compensate for already-initialized compositions.
+            // TODO: This assumes layer of composition type use a unique composition reference
+            var comp = this.root.compByID[layer.id];
+            var compGroup = game.make.group();
+            renderGroup.add(compGroup);
+            comp.init(game, compGroup);
+            this.skeletalContainer.addChild(comp.skeletalContainer);
+            compGroup.sort('z', Phaser.Group.SORT_DESCENDING);
+        }
+    }
+};
+
+AE.Composition.prototype.goToFrame = function(frameNum)
+{
+    for (var i=0; i < this.layers.length; i++)
+    {
+        var layer = this.layers[i];
+        layer.goToFrame(frameNum);
     }
 };
 
@@ -236,6 +305,7 @@ AE.Layer = function(xmlDom, root)
 {
     this.root = root;
     this.sprite = null; // the sprite that will be created to represent this layer, if any.
+    this.composition = null; // the composition that this layer is under
     // Copy any base attributes
     for (var i=0; i < xmlDom.attributes.length; i++)
     {
@@ -268,34 +338,88 @@ AE.Layer = function(xmlDom, root)
 
 AE.Layer.prototype.goToFrame = function(frameNum)
 {
+    if (!this.sprite)
+    {
+        return;
+    }
+
+    function getGlobalRotation(sprite)
+    {
+        var totalRotation = sprite.rotation;
+        var parent = sprite.parent;
+        while (parent)
+        {
+            totalRotation += parent.rotation;
+            parent = parent.parent;
+        }
+
+        return totalRotation;
+    }
     // Find Frame
     var keyframe = this.getFrameData(frameNum); // AE.Keyframe
 
     // Apply Frame
-    var sprite = this.sprite;
     if (keyframe.hasOwnProperty("Position"))
     {
-        sprite.x = keyframe.Position.x;
-        sprite.y = -keyframe.Position.y;
+        this.skeletalContainer.x = parseFloat(keyframe.Position.x);
+        this.skeletalContainer.y = parseFloat(keyframe.Position.y);
     }
 
     if (keyframe.hasOwnProperty("Rotation"))
     {
-        sprite.rotation = keyframe.Rotation.val;
-        if (sprite.rotation != 0)
-        {
-            console.log(sprite.rotation);
-        }
+        this.skeletalContainer.rotation = keyframe.Rotation.val / 180 * Math.PI;
     }
 
     if (keyframe.hasOwnProperty("Scale"))
     {
-        sprite.scale.set(keyframe.Scale.x / 200.0, keyframe.Scale.y / 200.0);
+        this.skeletalContainer.scale.set(keyframe.Scale.x / 100.0, keyframe.Scale.y / 100.0);
     }
 
     if (keyframe.hasOwnProperty("Anchor Point"))
     {
-        sprite.pivot.set(keyframe["Anchor Point"].x, keyframe["Anchor Point"].y);
+        this.skeletalContainer.pivot.set(keyframe["Anchor Point"].x, keyframe["Anchor Point"].y);
+        //this.sprite.pivot.set(this.skeletalContainer.pivot.x, this.skeletalContainer.pivot.y);
+    }
+
+    if (keyframe.hasOwnProperty("Opacity"))
+    {
+        this.skeletalContainer.alpha = keyframe.Opacity.val / 100.0;
+    }
+
+    if (this.skeletalContainer.parent)
+    {
+        var globalSkeletalPosition = this.skeletalContainer.world;
+        console.log(this.sprite.key + " - (" + globalSkeletalPosition.x + ", " + globalSkeletalPosition.y + ")");
+        if (this.sprite.parent)
+        {
+            var localDeltaVector = this.sprite.toLocal(globalSkeletalPosition);
+            this.sprite.position.x += localDeltaVector.x;
+            this.sprite.position.y += localDeltaVector.y;
+        }
+        else
+        {
+            this.sprite.position = globalSkeletalPosition;
+        }
+    }
+    else
+    {
+        this.sprite.position = this.skeletalContainer.position;
+    }
+
+    this.sprite.rotation = getGlobalRotation(this.skeletalContainer);
+    //this.sprite.scale.set(this.skeletalContainer.scale.x, this.skeletalContainer.scale.y);
+
+    this.sprite.alpha = this.skeletalContainer.worldAlpha;
+
+    if (this.type == "Composition")
+    {
+        // TODO: This doesn't compensate for already-initialized compositions.
+        // TODO: This assumes layer of composition type use a unique composition reference
+        var comp = this.root.compByID[this.id];
+        for (var i=comp.layers.length-1; i >= 0; i--)
+        {
+            comp.layers[i].goToFrame(frameNum);
+        }
     }
 };
 
