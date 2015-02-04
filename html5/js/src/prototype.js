eae99d85e76f1cd9a66d0a3c6b27f3c5d765ad65
Jerry Fu <jerry@instituteofplay.org>
2015-02-03 17:36:44 -0800

    Disabled items button unless you enter items=true in url query

--- html5/js/src/prototype.js
+++ html5/js/src/prototype.js
@@ -286,6 +286,7 @@ window.onload = function() {
             }
         }, this);
         bottomRightAnchor.addChild(uiElement);
+        uiElement.visible = GLOBAL.stickyMode = getParameterByName("items") == "true";
 
         game.input.onDown.add(globalDown, this); // Global input down handler
         game.input.onUp.add(globalUp, this); // Global input down handler
