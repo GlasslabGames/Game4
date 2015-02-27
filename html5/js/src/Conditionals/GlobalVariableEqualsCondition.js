3abe3a37ba6b2d7988bc0dd5dc1ae3c2c3181293
Rose Abernathy <rose@instituteofplay.org>
2015-02-27 15:54:52 -0800

    Fix journal tutorial to only show up if the journal is open.

---
+++ html5/js/src/Conditionals/GlobalVariableEqualsCondition.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Jerry Fu on 2/20/2015.
+ */
+
+var GlassLab = GlassLab || {};
+
+GlassLab.GlobalVariableEqualsCondition = function(variableName, compareTo)
+{
+    GlassLab.Conditional.prototype.constructor.call(this);
+
+    this.variableName = variableName;
+
+    this.target = compareTo;
+
+    this.updateSignalBinder = null;
+};
+
+GlassLab.GlobalVariableEqualsCondition.prototype = Object.create(GlassLab.Conditional.prototype);
+GlassLab.GlobalVariableEqualsCondition.prototype.constructor = GlassLab.GlobalVariableEqualsCondition;
+
+GlassLab.GlobalVariableEqualsCondition.prototype._calculateIsSatisfied = function()
+{
+    return GlassLab.Deserializer.getPropertyFromName(this.variableName) == this.target;
+};
+
+GlassLab.GlobalVariableEqualsCondition.prototype._doDestroy = function()
+{
+    if (this.updateSignalBinder)
+    {
+        GlassLab.SignalManager.update.remove(this._update, this);
+    }
+};
+
+GlassLab.GlobalVariableEqualsCondition.prototype._update = function(dt)
+{
+    this.Refresh();
+};
+
+GlassLab.GlobalVariableEqualsCondition.prototype.init = function()
+{
+    this.Refresh();
+
+    if (!this.isCompleted)
+    {
+        this.updateSignalBinder = GlassLab.SignalManager.update.add(this._update, this);
+    }
+};
