b3428d6123f71835b04c4457d55ce6727e27d5c7
Rose Abernathy <rose@instituteofplay.org>
2015-03-18 11:25:51 -0700

    Revert walkingToFood to get around a big bug with the pen going into the water (and it looks better)

--- html5/js/src/CreatureStates/CreatureStateWalkingToFood.js
+++ html5/js/src/CreatureStates/CreatureStateWalkingToFood.js
@@ -18,10 +18,13 @@ GlassLab.CreatureStateWalkingToFood.prototype.Enter = function()
 {
     GlassLab.CreatureState.prototype.Enter.call(this);
     this.speed = this.creature.moveSpeed - 0.25 + (Math.random() * 0.5); // adjust by +- 0.25
+    this.creature.PlayAnim("walk", true, this.speed * this.creature.baseAnimSpeed);
     this.creature.draggable = false;
-    this.creature.PathToTile(this.foodInfo.food.getTile());
 
+    /* If we use pathing for this - currently it looks weird and blocks the game if the pen goes into the water
+    this.creature.PathToTile(this.foodInfo.food.getTile());
     this.creature.onDestinationReached.add(this._onDestinationReached, this);
+    */
 };
 
 GlassLab.CreatureStateWalkingToFood.prototype.Exit = function()
@@ -29,15 +32,27 @@ GlassLab.CreatureStateWalkingToFood.prototype.Exit = function()
     GlassLab.CreatureState.prototype.Exit.call(this);
     this.creature.StopAnim();
 
-    this.creature.onDestinationReached.remove(this._onDestinationReached, this);
+    // this.creature.onDestinationReached.remove(this._onDestinationReached, this);
 };
 
 GlassLab.CreatureStateWalkingToFood.prototype.Update = function()
 {
-    this.creature._move();
+    var delta = Phaser.Point.subtract(GlassLab.Util.GetGlobalIsoPosition(this.foodInfo.food.sprite), GlassLab.Util.GetGlobalIsoPosition(this.creature.sprite));
+    if (delta.getMagnitudeSq() > Math.pow(GLOBAL.tileSize * 0.25, 2)) { // we're far from the carrot
+        delta.setMagnitude(this.speed);
+        Phaser.Point.add(this.creature.sprite.isoPosition, delta, delta);
+        this.creature.sprite.isoX = delta.x;
+        this.creature.sprite.isoY = delta.y;
+    }
+    else {
+        this.creature.StateTransitionTo(new GlassLab.CreatureStateEating(this.game, this.creature, this.foodInfo));
+    }
+    //this.creature._move(); // If we use pathing
 };
 
+/*
 GlassLab.CreatureStateWalkingToFood.prototype._onDestinationReached = function(creature)
 {
     this.creature.StateTransitionTo(new GlassLab.CreatureStateEating(this.game, this.creature, this.foodInfo));
 };
+*/
