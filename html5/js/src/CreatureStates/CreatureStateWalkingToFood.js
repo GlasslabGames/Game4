49aa54cbd8a94ace62eca83af5cd1b1ec7589354
Jerry Fu <jerry@instituteofplay.org>
2015-03-31 09:41:08 -0700

    Creatures run to food until close

--- html5/js/src/CreatureStates/CreatureStateWalkingToFood.js
+++ html5/js/src/CreatureStates/CreatureStateWalkingToFood.js
@@ -17,8 +17,19 @@ GlassLab.CreatureStateWalkingToFood.constructor = GlassLab.CreatureStateWalkingT
 GlassLab.CreatureStateWalkingToFood.prototype.Enter = function()
 {
     GlassLab.CreatureState.prototype.Enter.call(this);
-    this.speed = this.creature.moveSpeed - 0.25 + (Math.random() * 0.5); // adjust by +- 0.25
-    this.creature.PlayAnim("walk", true, this.speed * this.creature.baseAnimSpeed);
+    var delta = Phaser.Point.subtract(GlassLab.Util.GetGlobalIsoPosition(this.foodInfo.food.sprite), GlassLab.Util.GetGlobalIsoPosition(this.creature.sprite));
+
+    // When far, run
+    if (delta.getMagnitudeSq() < GLOBAL.tileSize * GLOBAL.tileSize * 1.5625) // 1.5625 = 1.25^2, derived from when creatures leave this state and begin to eat food (.25 squares away)
+    {
+        this.speed = this.creature.moveSpeed - .25 + (Math.random() * 0.5); // adjust by +- 0.25
+        this.creature.PlayAnim("walk", true, this.speed * this.creature.baseAnimSpeed);
+    }
+    else
+    {
+        this.speed = this.creature.moveSpeed + 5; // adjust by +- 0.25
+        this.creature.PlayAnim("walk", true, this.speed * this.creature.baseAnimSpeed);
+    }
     this.creature.draggable = false;
 
     /* If we use pathing for this - currently it looks weird and blocks the game if the pen goes into the water
@@ -38,11 +49,19 @@ GlassLab.CreatureStateWalkingToFood.prototype.Exit = function()
 GlassLab.CreatureStateWalkingToFood.prototype.Update = function()
 {
     var delta = Phaser.Point.subtract(GlassLab.Util.GetGlobalIsoPosition(this.foodInfo.food.sprite), GlassLab.Util.GetGlobalIsoPosition(this.creature.sprite));
-    if (delta.getMagnitudeSq() > Math.pow(GLOBAL.tileSize * 0.25, 2)) { // we're far from the carrot
+    var deltaMagSq = delta.getMagnitudeSq();
+    if (deltaMagSq > Math.pow(GLOBAL.tileSize * 0.25, 2)) { // we're far from the carrot
         delta.setMagnitude(this.speed);
         Phaser.Point.add(this.creature.sprite.isoPosition, delta, delta);
         this.creature.sprite.isoX = delta.x;
         this.creature.sprite.isoY = delta.y;
+
+        // When close and running, slow down
+        if ( this.speed >= this.creature.moveSpeed + .5 && deltaMagSq - this.speed*this.speed < GLOBAL.tileSize * GLOBAL.tileSize * 1.5625)
+        {
+            this.speed = this.creature.moveSpeed - .25 + (Math.random() * 0.5); // adjust by +- 0.25
+            this.creature.PlayAnim("walk", true, this.speed * this.creature.baseAnimSpeed);
+        }
     }
     else {
         this.creature.StateTransitionTo(new GlassLab.CreatureStateEating(this.game, this.creature, this.foodInfo));
