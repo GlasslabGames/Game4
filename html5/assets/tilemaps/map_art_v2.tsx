6211e86524ff91d64e8043a4cb095301257d93e3
Greg Bliss <greg.bliss@instituteofplay.org>
2015-04-22 15:07:27 -0700

    replaced all world tile art and recreated world map

---
+++ html5/assets/tilemaps/map_art_v2.tsx
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="map_art_v2" tilewidth="320" tileheight="290">
+ <tileoffset x="-60" y="0"/>
+ <tile id="0">
+  <image width="320" height="270" source="../images/tiles/world_art_v2/grass_blank_1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="320" height="270" source="../images/tiles/world_art_v2/grass_clover_1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="320" height="270" source="../images/tiles/world_art_v2/grass_clover_2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="320" height="270" source="../images/tiles/world_art_v2/grass_clover_3.png"/>
+ </tile>
+ <tile id="4">
+  <image width="320" height="280" source="../images/tiles/world_art_v2/forest_edge_left_1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="320" height="280" source="../images/tiles/world_art_v2/forest_edge_left_2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="320" height="290" source="../images/tiles/world_art_v2/forest_edge_left_3.png"/>
+ </tile>
+ <tile id="7">
+  <image width="320" height="250" source="../images/tiles/world_art_v2/forest_edge_right_1.png"/>
+ </tile>
+ <tile id="8">
+  <image width="320" height="230" source="../images/tiles/world_art_v2/forest_edge_right_2.png"/>
+ </tile>
+ <tile id="9">
+  <image width="320" height="230" source="../images/tiles/world_art_v2/forest_edge_right_3.png"/>
+ </tile>
+ <tile id="10">
+  <image width="320" height="230" source="../images/tiles/world_art_v2/forest_edge_inside_corner.png"/>
+ </tile>
+ <tile id="11">
+  <image width="320" height="230" source="../images/tiles/world_art_v2/forest_edge_outside_corner.png"/>
+ </tile>
+ <tile id="12">
+  <image width="320" height="270" source="../images/tiles/world_art_v2/forest_floor_1.png"/>
+ </tile>
+ <tile id="13">
+  <image width="320" height="240" source="../images/tiles/world_art_v2/forest_floor_2.png"/>
+ </tile>
+ <tile id="14">
+  <image width="320" height="120" source="../images/tiles/world_art_v2/pond_edge_back_corner.png"/>
+ </tile>
+ <tile id="15">
+  <image width="320" height="120" source="../images/tiles/world_art_v2/pond_edge_back_left.png"/>
+ </tile>
+ <tile id="16">
+  <image width="320" height="120" source="../images/tiles/world_art_v2/pond_edge_back_right.png"/>
+ </tile>
+ <tile id="17">
+  <image width="320" height="120" source="../images/tiles/world_art_v2/pond_edge_front_corner.png"/>
+ </tile>
+ <tile id="18">
+  <image width="320" height="120" source="../images/tiles/world_art_v2/pond_edge_front_left.png"/>
+ </tile>
+ <tile id="19">
+  <image width="320" height="120" source="../images/tiles/world_art_v2/pond_edge_front_right.png"/>
+ </tile>
+ <tile id="20">
+  <image width="320" height="120" source="../images/tiles/world_art_v2/pond_water_1.png"/>
+ </tile>
+ <tile id="21">
+  <image width="320" height="120" source="../images/tiles/world_art_v2/pond_water_2.png"/>
+ </tile>
+ <tile id="22">
+  <image width="320" height="120" source="../images/tiles/world_art_v2/pond_edge_back_point.png"/>
+ </tile>
+ <tile id="23">
+  <image width="320" height="120" source="../images/tiles/world_art_v2/pond_edge_cliff_left.png"/>
+ </tile>
+ <tile id="24">
+  <image width="320" height="120" source="../images/tiles/world_art_v2/pond_edge_cliff_right.png"/>
+ </tile>
+</tileset>
