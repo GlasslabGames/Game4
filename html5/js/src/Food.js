c05cde89f7eb5412d6d5b786e72319c6096566d6
Rose Abernathy <rose@instituteofplay.org>
2015-05-13 18:29:18 -0700

    Remove debug log

--- html5/js/src/Food.js
+++ html5/js/src/Food.js
@@ -361,7 +361,6 @@ GlassLab.Food.prototype.getTargets = function()
     if (!points.length) points = [new Phaser.Point(pos.x, pos.y - offset), new Phaser.Point(pos.x - offset)]; // in case no spots are left, default to the prefered points
 
     var priority = 1 + (this.eaters.length * 2); // food that already has creatures waiting to eat it gets a higher priority
-    console.log(this.eaters.length, priority);
 
     var targets = [];
     for (var i = 0; i < points.length; i++) {
